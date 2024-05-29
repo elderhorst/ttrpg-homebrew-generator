55e04d3f81dd4690b26773be990a92cdb7fca5d2
Michael Elderhorst <michael.elderhorst@gmail.com>
2024-05-29 13:37:32 -0700

    Removed text, markdown, and tsv files from the repo and it's history

--- Program.cs
+++ Program.cs
@@ -4,24 +4,24 @@ Console.WriteLine("Begining to parse files...");
 
 Parser parser = new Parser();
 
-parser.ParseSpellList("Elder Homebrew - Spell List.tsv");
-parser.ParseSpells("Elder Homebrew - Spells.tsv");
+parser.ParseSpellList("local/data/Spell List.tsv");
+parser.ParseSpells("local/data/Spells.tsv");
 parser.AddClassesToSpells();
-parser.ParseMagicItems("Elder Homebrew - Magic Items.tsv");
+parser.ParseMagicItems("local/data/Magic Items.tsv");
 
 Console.WriteLine("Completed parsing files...");
 Console.WriteLine("Generating document...");
 
 Generator generator = new Generator();
 
-generator.AddMarkdown("Pregenerated/Elder Introduction.md");
+generator.AddMarkdown("local/pregenerated/Introduction.md");
 generator.AddSpellList(parser.SpellList);
 generator.AddSpells(parser.Spells);
 generator.AddMagicItems(parser.MagicItems);
-generator.AddMarkdown("Pregenerated/Elder Heritages.md");
+generator.AddMarkdown("local/pregenerated/Heritages.md");
 
 Console.WriteLine("Saving document...");
 
-generator.SaveOutput("output.txt");
+generator.SaveOutput("local/output/output.txt");
 
 Console.WriteLine("Process completed.");
